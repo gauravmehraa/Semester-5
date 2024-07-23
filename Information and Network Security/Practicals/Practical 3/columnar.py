f4db51e7f1bd30bb534c8836101869ecef2f556a
gauravmehraa <gauravmehra158@gmail.com>
2024-07-24 00:40:58 +0530

    fixed columnar

--- Information and Network Security/Practicals/Practical 3/columnar.py
+++ Information and Network Security/Practicals/Practical 3/columnar.py
@@ -5,9 +5,14 @@ def getSequence(key):
   sequence = [sortedkey.index(char) for char in key]
   return sequence
 
+def getPermutation(key):
+  sorted_key = sorted(list(enumerate(key)), key=lambda x: x[1])
+  permutation = [_[0] for _ in sorted_key]
+  return permutation
+
 def encrypt(plaintext, key):
   ciphertext = ''
-  sequence = getSequence(key)
+  permutation = getPermutation(key)
   rows = math.ceil(len(plaintext) / len(key))
   cols = len(key)
   matrix = [['_' for _ in range(cols)] for _ in range(rows)]
@@ -15,16 +20,13 @@ def encrypt(plaintext, key):
   index = 0
   for i in range(rows):
     for j in range(cols):
-      if index >= len(plaintext): continue
-      matrix[i][j] = plaintext[index]
-      index += 1
+      if index < len(plaintext):
+        matrix[i][j] = plaintext[index]
+        index += 1
   
-  for _ in range(len(key)):
-    index = sequence.index(min(sequence)) # find index of smallest number
-    for row in matrix:
-      ciphertext += row[index]
-      del row[index] # remove element once added to CT
-    sequence.remove(min(sequence)) # remove smallest number
+  for num in permutation:
+    for i in range(rows):
+      ciphertext += matrix[i][num]
 
   return ciphertext
 
